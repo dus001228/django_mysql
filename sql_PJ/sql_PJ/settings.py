1e05d7b4cdd96cbf2997b77db7968cc6bc43fa88
dus001228 <dus001228@naver.com>
2023-01-03 09:57:38 +0900

    Update settings.py

--- sql_PJ/sql_PJ/settings.py
+++ sql_PJ/sql_PJ/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(wgsei6&^8lnrg%!a78wef8qc0m#_pt@0pta(0#6qtnxi1=cxa'
+SECRET_KEY = '비밀~'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
